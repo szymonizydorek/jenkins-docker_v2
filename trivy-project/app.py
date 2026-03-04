893141244fd3e90183585c163a12c1604c8c02cd
szymonizydorek <izydorek.szymon@gmail.com>
2026-03-04 17:37:00 +0000

    WIP: Test Trivy Options

--- trivy-project/app.py
+++ trivy-project/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello from Flask + Jenkins Lab!</h1>"
+    return "<h1>ello from Flask + Jenkins Lab!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
